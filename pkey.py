62e200d6c601be265d5af321bcfa5a36e2aa0a88
clonmac <clonmac@users.noreply.github.com>
2017-05-10 10:48:19 -0400

    Included more randomization or more rounds of sha256

--- pkey.py
+++ pkey.py
@@ -2,19 +2,16 @@ import hashlib
 
 first10 = ''
 specials = '!@#$%^&*()'
-count = 0
+rand_int = 0
 
-def hash_func(var):
+def hash_func(var):							#Hashing function
     hasher = 0
     hasher = hashlib.sha256()
     hasher.update(var)
     hashvalue = hasher.hexdigest()
     return hashvalue
 
-def rot(c,n):
-    """ rotate c forward by n characters,
-        wrapping as needed; only letters change
-    """
+def rot(c,n):								#Rotation function
     new_ord = ord(c) + (n % 26)
     if c.islower():
         if new_ord > ord('z'):
@@ -24,22 +21,31 @@ def rot(c,n):
         return chr(new_ord)
     return c
 
+def random_int(p):							#Random integer function
+	for n in p:
+		if n.isdigit():
+			print n
+			return int(n)
+
 domain = raw_input("Domain: ")
 master_pass = raw_input("Master Password: ")
 
-combined_pass = domain + master_pass
+hashed_domain = hash_func(domain)			#Hash domain
+hashed_master_pass = hash_func(master_pass)	#Hash master password
 
-password = hash_func(combined_pass)
+password = hashed_domain + hashed_master_pass
 
-for letter in password[0:9]:
-	first10 = first10 + rot(letter, 15)
+for i in range(256):
+	password = hash_func(password)			#Perform 256 rounds of sha256
 
-for i in range(9):
-	if first10[i].isdigit():
-		final_password = first10.replace(first10[i], specials[int(first10[i])])
-		break;
+rand_int = random_int(password)				#Set value of random integer
 
-final_password = final_password.upper() + password[10:18]
+for letter in password[rand_int:rand_int+10]:
+	first10 = first10 + rot(letter, rand_int + 10)
+
+final_password = first10.upper() + password[rand_int + 10:rand_int + 20]
+
+final_password = final_password.replace(final_password[rand_int + 10], specials[rand_int], 2)
 
 print final_password
 
